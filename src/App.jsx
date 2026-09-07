eb4430d0660913c7049274b475800d166391080e
luciano <luciano.delapena@gmail.com>
2026-09-07 17:38:27 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import "./App.css";
 import Plantillas from "./Plantillas";
-import { LanguageProvider } from "./Languagecontext";
+import { LanguageProvider } from "./LanguageContext";
 
 export default function App() {
     return (
