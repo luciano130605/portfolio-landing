e8a75bf5e042a99cea79166a8049d8ffad6f87dc
luciano <luciano.delapena@gmail.com>
2026-09-11 11:14:22 -0300

    fix

--- src/data.js
+++ src/data.js
@@ -27,7 +27,7 @@ export const header = {
     email: "luciano.delapena@gmail.com",
     whatsapp: "5491158161755",
     github: "https://github.com/luciano130605",
-    portfolio: "https://portfolio-gamma-puce-17.vercel.app/",
+    portfolio: "https://www.lucianodelapena.com/",
 };
 
 export const services = [
